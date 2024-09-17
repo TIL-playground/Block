789050f6ce89ebe5d341c6de04e6d89d4502e1d3
101 <tmddnjs001133@dsm.hs.kr>
2024-09-18 00:04:40 +0900

    add: contract

---
+++ PlatformToken.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PlatformToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("PlatformToken", "PTK") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
